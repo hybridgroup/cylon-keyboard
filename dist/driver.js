ad5eb34f1d104e508e0c49041a905b207dfddb87
xixebombilla <tiliches88@gmail.com>
2014-01-28 14:59:47 -0600

    130-tomdoc-fix-JS

--- dist/driver.js
+++ dist/driver.js
@@ -31,11 +31,6 @@
         return _ref;
       }
 
-      # Public: Starts the driver
-      #
-      # callback - params
-      #
-      # Returns null.
       Keyboard.prototype.start = function(callback) {
         var key, _i, _len, _ref1;
         Logger.info("Keyboard " + this.device.name + " starting.");
